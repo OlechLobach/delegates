bf627f2c5a58b09d86fe4f4378d3dbbd0cacf588
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-04-02 01:00:28 +0300

    initiall commit

--- delegates/Program.cs
+++ delegates/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace delegates
-{
-    internal class Program
+﻿using ArithmeticOperationsApp;
+
+namespace MainProject {
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ArithmeticChecker arithmeticChecker = new ArithmeticChecker();
+
+            Predicate<int> isEvenPredicate = new Predicate<int>(arithmeticChecker.IsEven);
+            Predicate<int> isOddPredicate = new Predicate<int>(arithmeticChecker.IsOdd);
+            Predicate<int> isPrimePredicate = new Predicate<int>(arithmeticChecker.IsPrime);
+            Predicate<int> isFibonacciPredicate = new Predicate<int>(arithmeticChecker.IsFibonacci);
+
+            int number = 7;
+
+            Console.WriteLine($"{number} is even: {isEvenPredicate(number)}");
+            Console.WriteLine($"{number} is odd: {isOddPredicate(number)}");
+            Console.WriteLine($"{number} is prime: {isPrimePredicate(number)}");
+            Console.WriteLine($"{number} is Fibonacci: {isFibonacciPredicate(number)}");
+
+            Console.ReadLine();
         }
     }
 }
---
+++ proj/ex_six.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArithmeticOperationsApp
+{
+    public class ArithmeticChecker
+    {
+        public bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+
+        public bool IsOdd(int number)
+        {
+            return number % 2 != 0;
+        }
+
+        public bool IsPrime(int number)
+        {
+            if (number <= 1)
+                return false;
+
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+        public bool IsFibonacci(int number)
+        {
+            return IsPerfectSquare(5 * number * number + 4) || IsPerfectSquare(5 * number * number - 4);
+        }
+
+        private bool IsPerfectSquare(int number)
+        {
+            int squareRoot = (int)Math.Sqrt(number);
+            return squareRoot * squareRoot == number;
+        }
+    }
+}
