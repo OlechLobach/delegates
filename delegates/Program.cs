8c4c1e4922de589dc54131210d2aebd304b52f84
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-04-02 00:41:07 +0300

    initiall commit

--- delegates/Program.cs
+++ delegates/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace delegates
+﻿using CreditCard;
+
+namespace MainProject
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Card creditCard = new Card("1234 5678 9012 3456", "John Doe", "12/24", 1234, 5000, 2000);
+
+            creditCard.AccountReplenishment += (sender, amount) =>
+            {
+                Console.WriteLine($"Account replenished by {amount}. Current balance: {creditCard.Balance}");
+            };
+            creditCard.MoneyWithdrawal += (sender, amount) =>
+            {
+                Console.WriteLine($"Money withdrawn: {amount}. Current balance: {creditCard.Balance}");
+            };
+            creditCard.CreditStart += (sender, amount) =>
+            {
+                Console.WriteLine($"Credit started. Current credit amount: {creditCard.CreditAmount}");
+            };
+            creditCard.CreditLimitReached += (sender, amount) =>
+            {
+                Console.WriteLine($"Credit limit reached: {creditCard.CreditLimit}. Current credit amount: {creditCard.CreditAmount}");
+            };
+            creditCard.PinChanged += (sender, newPin) =>
+            {
+                Console.WriteLine($"PIN changed. New PIN: {newPin}");
+            };
+
+            creditCard.ReplenishAccount(1000);
+            creditCard.WithdrawMoney(500);
+            creditCard.StartCredit(1500);
+            creditCard.WithdrawMoney(4000);
+            creditCard.ChangePin(4321);
+
+            Console.ReadKey();
         }
     }
 }
---
+++ proj/ex_third.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditCard
+{
+    public class Card
+    {
+        private string _cardNumber;
+        private string _cardholderName;
+        private string _expiryDate;
+        private int _pin;
+        private decimal _creditLimit;
+        private decimal _balance;
+
+        public event EventHandler<decimal> AccountReplenishment;
+        public event EventHandler<decimal> MoneyWithdrawal;
+        public event EventHandler<decimal> CreditStart;
+        public event EventHandler<decimal> CreditLimitReached;
+        public event EventHandler<int> PinChanged;
+
+        public Card(string cardNumber, string cardholderName, string expiryDate, int pin, decimal creditLimit, decimal initialBalance)
+        {
+            _cardNumber = cardNumber;
+            _cardholderName = cardholderName;
+            _expiryDate = expiryDate;
+            _pin = pin;
+            _creditLimit = creditLimit;
+            _balance = initialBalance;
+        }
+
+        public decimal Balance => _balance;
+        public decimal CreditAmount { get; private set; }
+        public decimal CreditLimit => _creditLimit;
+
+        public void ReplenishAccount(decimal amount)
+        {
+            _balance += amount;
+            AccountReplenishment?.Invoke(this, amount);
+        }
+
+        public void WithdrawMoney(decimal amount)
+        {
+            if (_balance >= amount)
+            {
+                _balance -= amount;
+                MoneyWithdrawal?.Invoke(this, amount);
+            }
+            else if (_balance + _creditLimit >= amount)
+            {
+                decimal remainingAmount = amount - _balance;
+                _balance = 0;
+                _creditLimit -= remainingAmount;
+                CreditAmount += remainingAmount;
+                MoneyWithdrawal?.Invoke(this, amount);
+                if (_creditLimit <= 0)
+                {
+                    CreditLimitReached?.Invoke(this, _creditLimit);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Insufficient funds.");
+            }
+        }
+
+        public void StartCredit(decimal amount)
+        {
+            _creditLimit -= amount;
+            CreditAmount += amount;
+            CreditStart?.Invoke(this, amount);
+        }
+
+        public void ChangePin(int newPin)
+        {
+            _pin = newPin;
+            PinChanged?.Invoke(this, newPin);
+        }
+    }
+}
