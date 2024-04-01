960a7bec82f8a320b150b272e79aab03c126fc53
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-04-02 00:27:18 +0300

    initiall commit

--- delegates/Program.cs
+++ delegates/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace delegates
+﻿using Helpers;
+
+namespace MainProject
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            TimeHelper.DisplayCurrentTime();
+            TimeHelper.DisplayCurrentDate();
+            TimeHelper.DisplayCurrentDayOfWeek();
+
+            Tuple<double, double, double> triangleSides = new Tuple<double, double, double>(3, 4, 5);
+            if (GeometryHelper.IsTriangleValid(triangleSides))
+            {
+                double triangleArea = GeometryHelper.CalculateTriangleArea(triangleSides);
+                Console.WriteLine($"Triangle area: {triangleArea}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid triangle sides.");
+            }
+
+            Tuple<double, double> rectangleSides = new Tuple<double, double>(4, 5);
+            double rectangleArea = GeometryHelper.CalculateRectangleArea(rectangleSides);
+            Console.WriteLine($"Rectangle area: {rectangleArea}");
+
+            Console.ReadKey();
         }
     }
 }
---
+++ proj/ex_second.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers
+{
+    public static class TimeHelper
+    {
+        public static void DisplayCurrentTime()
+        {
+            Console.WriteLine($"Current time: {DateTime.Now.ToShortTimeString()}");
+        }
+
+        public static void DisplayCurrentDate()
+        {
+            Console.WriteLine($"Current date: {DateTime.Now.ToShortDateString()}");
+        }
+
+        public static void DisplayCurrentDayOfWeek()
+        {
+            Console.WriteLine($"Current day of week: {DateTime.Now.DayOfWeek}");
+        }
+    }
+
+    public static class GeometryHelper
+    {
+        public static bool IsTriangleValid(Tuple<double, double, double> sides)
+        {
+            double a = sides.Item1;
+            double b = sides.Item2;
+            double c = sides.Item3;
+            return a + b > c && a + c > b && b + c > a;
+        }
+
+        public static double CalculateTriangleArea(Tuple<double, double, double> sides)
+        {
+            double a = sides.Item1;
+            double b = sides.Item2;
+            double c = sides.Item3;
+            double p = (a + b + c) / 2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+
+        public static double CalculateRectangleArea(Tuple<double, double> sides)
+        {
+            double a = sides.Item1;
+            double b = sides.Item2;
+            return a * b;
+        }
+    }
+}
