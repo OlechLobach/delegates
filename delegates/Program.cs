9202958271fd03c759e5889809d98a6d67047edd
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-04-02 01:03:48 +0300

    initiall commit

--- delegates/Program.cs
+++ delegates/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace delegates
+﻿using PracticalTaskApp;
+
+namespace MainProject
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            PracticalTaskExecutor executor = new PracticalTaskExecutor();
+
+            Action<string> displayAction = executor.DisplayMessage;
+            Func<double[], double> averageFunc = executor.CalculateAverage;
+            Func<double[], double> maxFunc = executor.FindMax;
+
+            double[] numbers = { 1.5, 2.7, 3.8, 4.1, 5.3 };
+
+            displayAction.Invoke("Current date:");
+            executor.DisplayCurrentDate.Invoke();
+            displayAction.Invoke("");
+
+            displayAction.Invoke("Current time:");
+            executor.DisplayCurrentTime.Invoke();
+            displayAction.Invoke("");
+
+            displayAction.Invoke("Average:");
+            double average = averageFunc.Invoke(numbers);
+            displayAction.Invoke(average.ToString());
+
+            displayAction.Invoke("");
+
+            displayAction.Invoke("Maximum:");
+            double max = maxFunc.Invoke(numbers);
+            displayAction.Invoke(max.ToString());
+
+            Console.ReadLine();
         }
     }
 }
---
+++ proj/seven.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalTaskApp
+{
+    public class PracticalTaskExecutor
+    {
+        public Action DisplayCurrentDate = () => Console.WriteLine(DateTime.Now.ToShortDateString());
+        public Action DisplayCurrentTime = () => Console.WriteLine(DateTime.Now.ToShortTimeString());
+
+        public void DisplayMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public double CalculateAverage(double[] numbers)
+        {
+            double sum = 0;
+            foreach (var num in numbers)
+            {
+                sum += num;
+            }
+            return sum / numbers.Length;
+        }
+
+        public double FindMax(double[] numbers)
+        {
+            double max = double.MinValue;
+            foreach (var num in numbers)
+            {
+                if (num > max)
+                {
+                    max = num;
+                }
+            }
+            return max;
+        }
+    }
+}
