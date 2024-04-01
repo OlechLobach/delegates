1e01705465aefb44db57962b712517d6f3fa276e
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-04-02 00:50:05 +0300

    initiall commit

--- delegates/Program.cs
+++ delegates/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace delegates
+﻿using MessageApp;
+
+
+namespace MainProject
+
 {
-    internal class Program
+    public delegate void DisplayMessageDelegate(string message);
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MessageDisplayer messageDisplayer = new MessageDisplayer();
+
+            DisplayMessageDelegate displayMessageDelegate = new DisplayMessageDelegate(messageDisplayer.DisplayMessage);
+
+            displayMessageDelegate("Hello, World!");
+            displayMessageDelegate("This is a delegate example.");
+            displayMessageDelegate("Goodbye!");
+
+            Console.ReadLine();
         }
     }
 }
---
+++ proj/ex_four.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+namespace MessageApp
+{
+    public class MessageDisplayer
+    {
+        public void DisplayMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
