73afd5975217e9253514713f0a3875297fa7d55f
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-04-02 00:12:35 +0300

    initiall commit

--- delegates/Program.cs
+++ delegates/Program.cs
@@ -1,10 +1,59 @@
-﻿namespace delegates
+﻿using System;
+using ArrayOperations;
+namespace ArrayOperations
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+            ArrayOperations operations = new ArrayOperations(array);
+
+            operations.EvenNumbersFound += (sender, result) =>
+            {
+                Console.WriteLine("Even numbers:");
+                foreach (var num in result)
+                {
+                    Console.Write(num + " ");
+                }
+                Console.WriteLine();
+            };
+            operations.GetEvenNumbers();
+
+            operations.OddNumbersFound += (sender, result) =>
+            {
+                Console.WriteLine("Odd numbers:");
+                foreach (var num in result)
+                {
+                    Console.Write(num + " ");
+                }
+                Console.WriteLine();
+            };
+            operations.GetOddNumbers();
+
+            operations.PrimeNumbersFound += (sender, result) =>
+            {
+                Console.WriteLine("Prime numbers:");
+                foreach (var num in result)
+                {
+                    Console.Write(num + " ");
+                }
+                Console.WriteLine();
+            };
+            operations.GetPrimeNumbers();
+
+            operations.FibonacciNumbersFound += (sender, result) =>
+            {
+                Console.WriteLine("Fibonacci numbers:");
+                foreach (var num in result)
+                {
+                    Console.Write(num + " ");
+                }
+                Console.WriteLine();
+            };
+            operations.GetFibonacciNumbers();
+
+            Console.ReadKey();
         }
     }
 }
---
+++ proj/ex_first.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayOperations
+{
+    public delegate void ArrayOperationEventHandler(object sender, List<int> result);
+
+    public class ArrayOperations
+    {
+        private int[] _array;
+
+        public event ArrayOperationEventHandler EvenNumbersFound;
+        public event ArrayOperationEventHandler OddNumbersFound;
+        public event ArrayOperationEventHandler PrimeNumbersFound;
+        public event ArrayOperationEventHandler FibonacciNumbersFound;
+
+        public ArrayOperations(int[] array)
+        {
+            _array = array;
+        }
+
+        public void GetEvenNumbers()
+        {
+            List<int> result = new List<int>();
+            foreach (var num in _array)
+            {
+                if (num % 2 == 0)
+                {
+                    result.Add(num);
+                }
+            }
+            EvenNumbersFound?.Invoke(this, result);
+        }
+
+        public void GetOddNumbers()
+        {
+            List<int> result = new List<int>();
+            foreach (var num in _array)
+            {
+                if (num % 2 != 0)
+                {
+                    result.Add(num);
+                }
+            }
+            OddNumbersFound?.Invoke(this, result);
+        }
+
+        public void GetPrimeNumbers()
+        {
+            List<int> result = new List<int>();
+            foreach (var num in _array)
+            {
+                if (IsPrime(num))
+                {
+                    result.Add(num);
+                }
+            }
+            PrimeNumbersFound?.Invoke(this, result);
+        }
+
+        private bool IsPrime(int num)
+        {
+            if (num <= 1)
+            {
+                return false;
+            }
+            for (int i = 2; i <= Math.Sqrt(num); i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public void GetFibonacciNumbers()
+        {
+            List<int> result = new List<int>();
+            int a = 0, b = 1;
+            while (b <= _array[_array.Length - 1])
+            {
+                if (Array.IndexOf(_array, b) != -1)
+                {
+                    result.Add(b);
+                }
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            FibonacciNumbersFound?.Invoke(this, result);
+        }
+    }
+}
