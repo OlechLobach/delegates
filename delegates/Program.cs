83c2da10a9925b31e773fd9022382e094b02035e
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-04-02 00:56:46 +0300

    initiall commit

--- delegates/Program.cs
+++ delegates/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace delegates
-{
-    internal class Program
+﻿using ArithmeticApp;
+
+namespace MainProject{
+    public delegate int ArithmeticOperationDelegate(int x, int y);
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Calculator calculator = new Calculator();
+
+            ArithmeticOperationDelegate addDelegate = new ArithmeticOperationDelegate(calculator.Add);
+            ArithmeticOperationDelegate subtractDelegate = new ArithmeticOperationDelegate(calculator.Subtract);
+            ArithmeticOperationDelegate multiplyDelegate = new ArithmeticOperationDelegate(calculator.Multiply);
+
+            int x = 10;
+            int y = 5;
+
+            Console.WriteLine($"{x} + {y} = {addDelegate(x, y)}");
+            Console.WriteLine($"{x} - {y} = {subtractDelegate(x, y)}");
+            Console.WriteLine($"{x} * {y} = {multiplyDelegate(x, y)}");
+
+            Console.ReadLine();
         }
     }
 }
